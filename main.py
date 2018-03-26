163bdf1b698feaf493276bee4bfde1035fb80a4e
Nigel Schuster <nigel.schusters@googlemail.com>
2018-03-25 20:39:24 -0400

    Handle simple class creation

--- main.py
+++ main.py
@@ -4,22 +4,47 @@ import json
 import re
 import os
 import sys
-from types import FunctionType, MethodType, LambdaType
+
+KNOWN_CONSTRUCTORS = {}
+
+
+def handle_value(val, imports):
+    if type(val) == dict:
+        return {k: handle_value(v, imports) for k, v in val.items()}
+    if type(val) == list:
+        return "[" + ", ".join([handle_value(v, imports) for v in val]) + "]"
+    if type(val) == tuple:
+        return "[" + ", ".join([handle_value(v, imports) for v in val]) + "]"
+    if type(val) in (int, float, bool):
+        return str(val)
+    if type(val) == str:
+        return "'{}'".format(val)
+    if hasattr(val, '__class__'):
+        if id(val) in KNOWN_CONSTRUCTORS:
+            return "NOT IMPLEMENTED YET"
+        else:
+            print(val.__class__)
+            imports.append("import {}".format(val.__class__.__module__))
+            return "{}.{}()".format(val.__class__.__module__,
+                                    val.__class__.__name__)
 
 
 def main():
     def method_wrapper_outer(fnc, module_title, class_name, test_path):
         def method_wrapper(*args, **kwargs):
-            in_args = str(args)
-            in_kwargs = str(kwargs)
+            imports = []
+            in_args = handle_value(args, imports)
+            print(in_args)
+            in_kwargs = handle_value(kwargs, imports)
             test_path_parent = os.path.abspath(
                 os.path.join(test_path, os.pardir))
             if not os.path.exists(test_path_parent):
                 os.makedirs(test_path_parent)
             file_handle = open(test_path, "w+")
-            res = fnc(*args, **kwargs)
+            res = handle_value(fnc(*args, **kwargs), imports)
             file_handle.write("""import unittest
 from {} import {}
+{}
 
 
 class Test{}(unittest.TestCase):
@@ -30,8 +55,8 @@ class Test{}(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-""".format(module_title, class_name, class_name, fnc.__name__, class_name,
-            fnc.__name__, in_args, in_kwargs, res))
+""".format(module_title, class_name, "\n".join(imports), class_name,
+            fnc.__name__, class_name, fnc.__name__, in_args, in_kwargs, res))
             return res
 
         return method_wrapper
@@ -60,7 +85,7 @@ if __name__ == '__main__':
                 setattr(class_obj, method_name,
                         method_wrapper_outer(func, module_title, class_name,
                                              test_path))
-    helperstuff.HelperClass.my_func([55])
+    helperstuff.HelperClass().my_func([55])
 
 
 if __name__ == '__main__':

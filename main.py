326885109bc528e1f0b7ee0cf8e5e2a11a64f299
Nigel Schuster <nigel.schusters@googlemail.com>
2018-03-25 23:05:30 -0400

    Constructor with args

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ KNOWN_CONSTRUCTORS = {}
 
 
 def handle_value(val, imports):
+    if val is None:
+        return "None"
     if type(val) == dict:
         return {k: handle_value(v, imports) for k, v in val.items()}
     if type(val) == list:
@@ -20,11 +22,13 @@ def handle_value(val, imports):
     if type(val) == str:
         return "'{}'".format(val)
     if hasattr(val, '__class__'):
-        if id(val) in KNOWN_CONSTRUCTORS:
-            return "NOT IMPLEMENTED YET"
+        imports.append("import {}".format(val.__class__.__module__))
+        if id(val) in KNOWN_CONSTRUCTORS.keys():
+            arg_data = KNOWN_CONSTRUCTORS[id(val)]
+            return "{}.{}(*{}, **{})".format(
+                val.__class__.__module__, val.__class__.__name__,
+                arg_data["args"], arg_data["kwargs"])
         else:
-            print(val.__class__)
-            imports.append("import {}".format(val.__class__.__module__))
             return "{}.{}()".format(val.__class__.__module__,
                                     val.__class__.__name__)
 
@@ -33,15 +37,22 @@ def main():
     def method_wrapper_outer(fnc, module_title, class_name, test_path):
         def method_wrapper(*args, **kwargs):
             imports = []
-            in_args = handle_value(args, imports)
-            print(in_args)
             in_kwargs = handle_value(kwargs, imports)
+            if fnc.__name__ == "__init__":
+                in_args = handle_value(args[1:], imports)
+                KNOWN_CONSTRUCTORS[id(args[0])] = {
+                    "args": in_args,
+                    "kwargs": in_kwargs,
+                }
+            else:
+                in_args = handle_value(args, imports)
             test_path_parent = os.path.abspath(
                 os.path.join(test_path, os.pardir))
             if not os.path.exists(test_path_parent):
                 os.makedirs(test_path_parent)
             file_handle = open(test_path, "w+")
-            res = handle_value(fnc(*args, **kwargs), imports)
+            return_value = fnc(*args, **kwargs)
+            res = handle_value(return_value, imports)
             file_handle.write("""import unittest
 from {} import {}
 {}
@@ -57,7 +68,7 @@ if __name__ == '__main__':
     unittest.main()
 """.format(module_title, class_name, "\n".join(imports), class_name,
             fnc.__name__, class_name, fnc.__name__, in_args, in_kwargs, res))
-            return res
+            return return_value
 
         return method_wrapper
 
@@ -85,7 +96,7 @@ if __name__ == '__main__':
                 setattr(class_obj, method_name,
                         method_wrapper_outer(func, module_title, class_name,
                                              test_path))
-    helperstuff.HelperClass().my_func([55])
+    helperstuff.HelperClass(8).my_func([55])
 
 
 if __name__ == '__main__':
